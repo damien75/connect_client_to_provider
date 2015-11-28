ef19d21f1eb780b5ca6d776fa96697a0862707e9
Damien Goblot <damiengoblot@gmail.com>
2015-11-28 17:10:18 -0500

    Adding a bit of style to the app. Displaying all specialties given from the controller in checkboxes Displaying all statuses from controller in a select panel. TODO: form_tag should redirect to provider that was just created rather than providers list, to check the information once again

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,6 +1,6 @@
 class ProvidersController < ApplicationController
     def new
         @all_statuses = ['private' , 'public']
-	@all_specialties = ['specialty 1' , 'specialty 2' , 'specialty 3']
+	    @all_specialties = ['specialty 1' , 'specialty 2' , 'specialty 3']
     end
 end
