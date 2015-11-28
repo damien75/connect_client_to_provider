db28de432cb76c23427fbffd109fb344524a69d5
damien75 <damiengoblot@gmail.com>
2015-11-28 22:54:45 +0000

    Adding an index page for the providers list

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -3,4 +3,8 @@ class ProvidersController < ApplicationController
         @all_statuses = ['private' , 'public']
 	    @all_specialties = ['specialty 1' , 'specialty 2' , 'specialty 3']
     end
+    
+    def index
+        @providers = Provider.all
+    end
 end
